0a357d4f89fcc51e963836aceff8865b1f223bab
BSIT2A-abas <rabas_220000001580@uic.edu.ph>
2024-09-27 21:35:54 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RootWidget());
+}
+
+class RootWidget extends StatelessWidget{
+    const RootWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+
+}
+
